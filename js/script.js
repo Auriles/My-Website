9c3ccc845fcfa51e39a69b21e7ba3e37d28c27ae
Auriles El Haddad <auriles0404@gmail.com>
2017-03-26 23:20:01 +0200

    adding images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    $(".navbar a, footer a#aboutFooter").on("click", function(event){
+    $(".navbar a.scroll, footer a#aboutFooter").on("click", function(event){
         event.preventDefault();
         var hash = this.hash;
 
